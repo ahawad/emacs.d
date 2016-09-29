d3508d64e5f4aced1bb91fcff9f2135d48626cf4
Ahmed Awad <b00042183@aus.edu>
2016-09-29 11:53:46 +0100

    Removed elpa/ from tracking

--- elpa/epl-20150517.433/epl-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; epl-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("epl.el") (22508 59970 893298 910000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; epl-autoloads.el ends here
